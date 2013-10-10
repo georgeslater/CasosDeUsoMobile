58b95d9c2d12cb15a565ae6d819d0061cf86915e
George Slater <georgewslater@gmail.com>
2013-10-09 22:27:54 -0200

    anda?

--- src/com/example/android/Constantes.java
+++ src/com/example/android/Constantes.java
@@ -14,8 +14,8 @@ public class Constantes {
     public static final String CUSTOM_LOG_TAG = "GEORGE";
     
     //servicios web
-    //public static final String DIRECCION_IP_ESTATICA = "http://10.0.2.2";
-    public static final String DIRECCION_IP_ESTATICA = "http://192.168.1.45";    
+    public static final String DIRECCION_IP_ESTATICA = "http://10.0.2.2";
+    //public static final String DIRECCION_IP_ESTATICA = "http://192.168.1.45";    
     //public static final String DIRECCION_IP_ESTATICA = "http://10.0.2.2";
     public static final String PUERTA = ":8080";
     public static final String SYNC_IMAGENES_URL = Constantes.DIRECCION_IP_ESTATICA+Constantes.PUERTA+"/CasosDeUso5/webresources/casosdeusowsport/"+Constantes.OBTENER_IMAGENES_REST_RESOURCE_URL+"?"+Constantes.USUARIO_ID_PARAM+"=";
